ce396bc475cf075b808918fd026d4a3175f9596b
giftnadia <nadiababale2@gmail.com>
2019-01-23 16:54:01 +0300

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+def age_calc(yearofbirth):
+   age=("2019-year_of_birth")
+
+    if age < 18 
+    print("your are a minor and you are () years old".format())
+
+    elif age < 35
+    print("you are a youth and you are () years old".format())
+
+    else age > 35
+    print("you are an elder and you are () years old".format())
+
+year of birth=int(input("year_of_birth"))
+print(age_calc)
