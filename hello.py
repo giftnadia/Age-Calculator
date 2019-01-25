8ae0aad6b55e8c1c318e728949ca1a98bb8ac763
giftnadia <nadiababale2@gmail.com>
2019-01-25 17:36:36 +0300

    create an age calculator

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
+year_of_birth=int(input("year_of_birth"))
+
 def age_calc(yearofbirth):
-   age=("2019-year_of_birth")
+   age=(2019-year_of_birth)
 
-    if age < 18 
-    print("your are a minor and you are () years old".format())
+   if age < 18 :
+       print("your are a minor")
 
-    elif age < 35
-    print("you are a youth and you are () years old".format())
+   elif age < 35 :
+       print("you are a youth")
 
-    else age > 35
-    print("you are an elder and you are () years old".format())
+   else :
+       print("you are an elder")
 
-year of birth=int(input("year_of_birth"))
-print(age_calc)
+age_calc(year_of_birth)
